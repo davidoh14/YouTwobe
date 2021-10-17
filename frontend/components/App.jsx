99def8a284cedddb440e8b1a9097ee3ebb612e68
David Oh <davidohyoung@gmail.com>
2021-10-16 23:06:54 -0500

    static pages render hello

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,29 +1,28 @@
-import React from "react";
+// import React from "react";
 // import login_form_container from "./session/login_form_container";
 // import signup_form_container from "./session/signup_form_container";
 // import { AuthRoute, ProtectedRoute } from "../util/route_util";
-import { Route, Switch } from "react-router";
+// import { Route, Switch } from "react-router";
 // import Router from "react-router-dom";
 // import PageNotFound from "./session/page_not_found";
 // import Home from "./home";
-// import VideoShowContainer from "./videos/video_show_container";
+// import VideoShowContainer from "./videos/video_show_container"
 // import VideoUploadFormContainer from "./videos/video_upload_form_container";
-import Root from "./root";
 
-const App = () => (
-  <div>
-    <header>
-      <Switch>
-        {/* <ProtectedRoute path="/upload/" component={VideoUploadFormContainer} />
-        <Route path="/watch/:videoId" component={VideoShowContainer} />
-        <AuthRoute path="/login" component={login_form_container} />
-        <AuthRoute path="/signup" component={signup_form_container} />
-        <Route exact path="/" component={Home} />
-        <Route component={PageNotFound} /> */}
-        <Route exact path='/' component={Root}/>
-      </Switch>
-    </header>
-  </div>
-);
 
-export default App;
+// const App = () => (
+//   <div>
+//     <header>
+//       <Switch>
+//         <ProtectedRoute path='/upload/' component={VideoUploadFormContainer}/>
+//         <Route path='/watch/:videoId' component={VideoShowContainer}/>
+//         <AuthRoute path="/login" component={login_form_container} />
+//         <AuthRoute path="/signup" component={signup_form_container} />
+//         <Route exact path='/' component={Home} />
+//         <Route component={PageNotFound} />
+//       </Switch>
+//     </header>
+//   </div>
+// );
+
+// export default App;
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import configureStore from "./store/store";
-import Root from "./components/root";
-import { login, logout, signup } from "./actions/session_actions";
+// import configureStore from "./store/store";
+// import Root from "./components/root";
+// import { login, logout, signup } from "./actions/session_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
