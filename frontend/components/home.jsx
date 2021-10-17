4a9271037271927ac425a862963df3777ae0d4dd
David Oh <davidohyoung@gmail.com>
2021-10-17 00:24:24 -0500

    commented out nav bar container

--- frontend/components/home.jsx
+++ frontend/components/home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-import NavBarContainer from "./nav/nav_bar_container";
+// import NavBarContainer from "./nav/nav_bar_container";
 // import VideoIndexContainer from "./videos/video_index_container";
 
 class Home extends React.Component {
   render() {
     return (
       <div>home
-        <NavBarContainer />
+        {/* <NavBarContainer /> */}
         {/* <VideoIndexContainer /> */}
       </div>
     );
