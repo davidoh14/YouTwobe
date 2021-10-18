2a3d11cf85c3f2a131759a91302bde3440d1fff2
David Oh <davidohyoung@gmail.com>
2021-10-18 00:50:47 -0500

    protec me

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
 
+    protect_from_forgery with: :exception
+
     helper_method :current_user
   
     def current_user 
