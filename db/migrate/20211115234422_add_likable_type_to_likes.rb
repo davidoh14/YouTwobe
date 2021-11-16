828c4eee3ae4a96d4aaebbbfca7eb976cc3b6787
David Oh <davidohyoung@gmail.com>
2021-11-15 18:17:57 -0600

    fixed likes migration

--- db/migrate/20211115234422_add_likable_type_to_likes.rb
+++
@@ -1,5 +0,0 @@
-class AddLikableTypeToLikes < ActiveRecord::Migration[5.2]
-  def change
-    add_column :likes, :likable, :references, null: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_225625) do
+ActiveRecord::Schema.define(version: 2021_11_16_000444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,9 @@ ActiveRecord::Schema.define(version: 2021_11_09_225625) do
     t.string "type", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "likable_type", null: false
+    t.bigint "likable_id", null: false
+    t.index ["likable_type", "likable_id"], name: "index_likes_on_likable_type_and_likable_id"
   end
 
   create_table "users", force: :cascade do |t|
