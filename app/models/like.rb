07a7a2ea0bcae5e8236db3f83d8732d25496a6cb
David Oh <davidohyoung@gmail.com>
2021-11-16 19:39:19 -0600

    likable type validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,13 @@
 class Like < ApplicationRecord
 
     validates :liker_id,
-        uniqueness: { scope: :video_id}
+        uniqueness: { scope: [:likable_id, :likable_type]}
+
+    validates :likable_type,
+        inclusion: { in: %w(video comment), message: '%{value} cannot be liked or disliked. Must be a video or comment.'}
 
     validates :type,
-        inclusion: { in: %w(like dislike), message: '%{value} is not a valid like type. Must be like or dislike'}
+        inclusion: { in: %w(like dislike), message: '%{value} is not a valid like type. Must be like or dislike.'}
 
     belongs_to :user,
         foreign_key: :liker_id,
