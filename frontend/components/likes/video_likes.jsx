b28217e023cb76b2ef3dfbe82840203cee743b71
David Oh <davidohyoung@gmail.com>
2021-11-19 19:04:14 -0600

    like and dislike component if then logic

--- frontend/components/likes/video_likes.jsx
+++ frontend/components/likes/video_likes.jsx
@@ -7,25 +7,43 @@ import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
 
 
 class VideoLikes extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {};
+    //     this.handleLike = this.handleLike.bind(this);
+    // };
+
     render () {
 
         const { video } = this.props;
 
-        if (video.currentLike) {
-            like = <ThumbUpAltIcon onClick={() => console.log("click")}/>;
+        let like;
+
+        if (video.currentLike && video.currentLike.kind === "like") {
+            // if like exists and kind = like, show a filled like button with action to delete like
+            like = <ThumbUpAltIcon onClick={() => console.log("delete like")} />; 
+        } else if (video.currentLike && video.currentLike.kind === "dislike"){
+            // if like exists but is kind = dislike, show unfilled like with action to post like
+            like = <ThumbUpOffAltIcon onClick={() => console.log("patch dislike to like")}/>;
         } else {
-            like = <ThumbUpOffAltIcon></ThumbUpOffAltIcon>;
+            // if like doesn't exist, show unfilled like with action to create like
+            like = <ThumbUpOffAltIcon onClick={() => console.log("create like")} />;
         }
 
-        // if (video.currentLike) {
-        //   Like = <ThumbUpAltIcon></ThumbUpAltIcon>;
-        // } else {
-        //   Like = <ThumbUpOffAltIcon></ThumbUpOffAltIcon>;
-        // }
+        let dislike;
 
-        if (!video) {
-            return null;
+        if (video.currentLike && video.currentLike.kind === "dislike") {
+            // if dislike exists and kind = dislike, show a filled dislike button with action to delete dislike
+            dislike = <ThumbDownAltIcon onClick={() => console.log("delete dislike")}/>;
+        } else if (
+            video.currentLike && video.currentLike.kind === "like") {
+            // if dislike exists but is kind = like, show unfilled dislike with action to post dislike
+            dislike = <ThumbDownOffAltIcon onClick={() => console.log("patch like to dislike")}/>;
+        } else {
+            // if dislike doesn't exist, show unfilled dislike with action to create dislike
+            dislike = <ThumbDownOffAltIcon onClick={() => console.log("create dislike")}/>;
         }
+
 
         return (
           <div className="show-likes">
@@ -33,9 +51,11 @@ class VideoLikes extends React.Component {
 
             {video.likesLength}
 
-            <ThumbDownAltIcon onClick={() => console.log("click2")} />
+            {dislike}
 
             {video.dislikesLength}
+
+            
           </div>
         );
     }
