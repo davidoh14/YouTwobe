37afc2b7a01bb0a8ccfd3498d2f87bdee8d25163
David Oh <davidohyoung@gmail.com>
2021-12-02 19:12:58 -0600

    parse search term

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -4,52 +4,58 @@ import VideoRow from "./video_row"
 import { useState, useEffect } from "react"
 
 export const Search = ({ currentUser, videos, fetchVideos, searchTerm }) => {
+
   const [filteredVideos, setFilteredVideos] = useState([]);
+  const [parsedSearchTerm, setParsedSearchTerm] = useState([]);
 
   useEffect(() => {
     fetchVideos();
+    parse();
   }, []);
 
   useEffect(() => {
     if (videos) {
-      setFilteredVideos((filteredVideos) => [
-        ...filteredVideos,
-        ...videos,
-      ]);
+      // setFilteredVideos((filteredVideos) => [
+      //   ...filteredVideos,
+      //   ...videos,
+      // ]);
+      // parse()
     }
   }, [videos]);
 
-  console.log(videos);
-
   // useEffect(() => {
-  // }, [videos])
+  //   filterVideos()
+  // }, [parsedSearchTerm])
 
-  // let parsedSearchTerm;
-
-  // function parse(){
-  //     let slicedSearchTerm = searchTerm.slice(8)
-  //     // let parsedSearchTerm = slicedSearchTerm.split(" ")
-  //     console.log('sliced', slicedSearchTerm)
-  // }
+  function parse(){
+      let splitSearchTerm;
+      let slicedSearchTerm = searchTerm.slice(9)
+      splitSearchTerm = slicedSearchTerm.split(" ")
+      setParsedSearchTerm((parsedSearchTerm) => [...parsedSearchTerm, ...splitSearchTerm])
+  }
 
   // function filterVideos() {
-  //     videos.filter(video => video.title)
+  //     let filteredVids = [];
+  //     let filtVids;
+      
+  //     parsedSearchTerm.forEach(term => {
+  //       filtVids = videos.filter(video => 
+  //         video.title.includes(term) ||
+  //         video.description.includes(term) ||
+  //         video.uploader.username.includes(term))
+  //     })
+
+  //     filteredVids.push(filtVids)
+  //     setFilteredVideos((filteredVids) => [...filteredVids]);      
   // }
 
-  // console.log('searchTerm', searchTerm)
-  // console.log('parsed', parsedSearchTerm)
-  // console.log('videos', videos)
-
-  // console.log('videos', videos)
-  // console.log('filtered', filteredVideos)
-
   if (filteredVideos === []) {
-    console.log("anything out there");
     return null;
   } else {
     return (
       <div>
-        {console.log("return", filteredVideos)}
+        {console.log('parsed search term', parsedSearchTerm)}
+        {/* {console.log('return', filteredVideos)} */}
         <NavBarContainer></NavBarContainer>
       </div>
     );
